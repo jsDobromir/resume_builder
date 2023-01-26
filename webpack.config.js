1f8bf010d46966b50158708388b9a74706a1e38b
Dobromir Ganchev <dobrganch89@gmail.com>
2023-01-26 21:18:01 +0200

    proj update

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isProduction = false;
+const isProduction = true;
 
 const config = {
   entry: {
